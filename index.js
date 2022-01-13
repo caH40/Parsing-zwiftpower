979346b6aa057db0e7172ae3cfe36d7b280fdbc8
caH40 <kmvsancho@gmail.com>
2022-01-13 21:05:35 +0300

    removed unnecessary characters

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
-const fs = require('fs')
+const fs = require('fs');
 
 //функция обёртка для синхронных операций
 async function start() {
@@ -16,8 +17,10 @@ async function start() {
 		});
 		const link = 'https://zwiftpower.com/events.php?zid=2619563';
 		await page.goto(link, { waitUntil: 'domcontentloaded' });
-		await page.$eval('#username', (elem) => elem.value = 'Sanchous');
-		await page.$eval('#password', (elem) => elem.value = 'abkbfk.nr');
+		const user = process.env.USER;
+		const password = process.env.PASSWORD;
+		await page.$eval('#username', (elem, user) => (elem.value = user), user);
+		await page.$eval('#password', (elem, password) => (elem.value = password), password);
 		await page.click('#login > fieldset > div.row.mobile-fix > div:nth-child(1) > input.btn.btn-success.btn-block.btn-lg');
 		await page.waitForTimeout(6000);
 
@@ -31,22 +34,24 @@ async function start() {
 				resultThead.push(element.innerText.split(' ').join(''))
 			});
 			//определение номеров столбцов с нужными данными
+			//weightColumn определяется как номер колонки среднего пульса -1
 			const theadNumber = {
 				riderColumn: resultThead.indexOf('Rider') + 1,
 				timerColumn: resultThead.indexOf('Time') + 1,
 				avgWKgColumn: resultThead.indexOf('avg') + 1,
 				avgWattsColumn: resultThead.indexOf('watts') + 1,
-				twentyMinWattsColumn: resultThead.indexOf('20m') + 1,
-				fiveMinWattsColumn: resultThead.indexOf('5m') + 1,
-				oneMinWattsColumn: resultThead.indexOf('1m') + 1,
-				thirtySeсWattsColumn: resultThead.indexOf('30s') + 1,
-				fifteenSeсWattsColumn: resultThead.indexOf('15s') + 1,
+				twentyMinWKgColumn: resultThead.indexOf('20m') + 1,
+				fiveMinWKgColumn: resultThead.indexOf('5m') + 1,
+				oneMinWKgColumn: resultThead.indexOf('1m') + 1,
+				thirtySeсWKgColumn: resultThead.indexOf('30s') + 1,
+				fifteenSeсWKgColumn: resultThead.indexOf('15s') + 1,
+				weightColumn: resultThead.indexOf('Avg'),
 				avrHeartColumn: resultThead.indexOf('Avg') + 1,
 				maxHeartColumn: resultThead.indexOf('Max') + 1
 			}
 			// console.log(theadNumber);
 
-			let result = [];
+			let result = [{ timeScraping: new Date().toLocaleString() }];
 			let container = await document.querySelectorAll('#table_event_results_final > tbody > tr')
 			let i = 0;
 
@@ -63,19 +68,48 @@ async function start() {
 				}
 				let riderLinkZp = element.querySelector('a').href;
 				let timeFull = element.querySelector('.pull-left').innerText;
-				let timeGap = element.querySelector('.pull-right').innerText;
-				let avgWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avgWKgColumn})`).innerText;
-				let avgWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avgWattsColumn})`).innerText;
-				let twentyMinWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.twentyMinWattsColumn})`).innerText;
-				let fiveMinWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.fiveMinWattsColumn})`).innerText;
-				let oneMinWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.oneMinWattsColumn})`).innerText;
-				let thirtySeсWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.thirtySeсWattsColumn})`).innerText;
-				let fifteenSeсWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.fifteenSeсWattsColumn})`).innerText;
-				let avrHeart = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avrHeartColumn})`).innerText;
-				let maxHeart = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.maxHeartColumn})`).innerText;
+				let timeGap = element.querySelector('.pull-right')
+					.innerText.split('+').join('').split('s').join('');
+				let avgWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avgWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let avgWatts = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avgWattsColumn})`)
+					.innerText.split('w').join('');
+				let twentyMinWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.twentyMinWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let fiveMinWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.fiveMinWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let oneMinWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.oneMinWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let thirtySeсWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.thirtySeсWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let fifteenSeсWKg = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.fifteenSeсWKgColumn})`)
+					.innerText.split('w/kg').join('');
+				let weight = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.weightColumn})`)
+					.innerText.split('kg').join('');
+				let avrHeart = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.avrHeartColumn})`)
+					.innerText.split('bpm').join('');
+				let maxHeart = element.querySelector(`#table_event_results_final > tbody > tr:nth-child(${i}) > td:nth-child(${theadNumber.maxHeartColumn})`)
+					.innerText.split('bpm').join('');
 
 
-				result.push({ category, rider, riderTeam, riderLinkZp, timeFull, timeGap, avgWKg, avgWatts, twentyMinWatts, fiveMinWatts, oneMinWatts, thirtySeсWatts, fifteenSeсWatts, avrHeart, maxHeart })
+				result.push({
+					category,
+					rider,
+					riderTeam,
+					riderLinkZp,
+					timeFull,
+					timeGap,
+					avgWKg,
+					avgWatts,
+					twentyMinWKg,
+					fiveMinWKg,
+					oneMinWKg,
+					thirtySeсWKg,
+					fifteenSeсWKg,
+					weight,
+					avrHeart,
+					maxHeart
+				})
 			});
 			return result
 		})
@@ -84,7 +118,7 @@ async function start() {
 			if (error) {
 				throw error
 			}
-			console.log('Saved zwiftpower.json');
+			console.log(`Zwiftpower.json saved ${table[0].timeScraping}`);
 		})
 
 
